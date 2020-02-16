cb0302e32ec360bb6a5e1a221aba295a8dc3a260
Isaac <aisaacvalle@gmail.com>
2020-02-16 13:01:13 -0600

    rest server-usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,16 @@
 //definicion del puerto 
 process.env.PORT = process.env.PORT || 3000;
+
+//entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//base de datos
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+  urlDB = 'mongodb+srv://alberto:YIQMTnZGLjRngqKi@cluster0-ctcpe.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+
+process.env.URL_DB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,43 +1,27 @@
 require('./config/config');
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
 app.use(bodyParser.json());
- 
-app.get('/usuario', function (req, res) {
-  res.json('get Usuario');
-});
 
-app.post('/usuario', function (req, res) {
-  let body = req.body;
-  if (body.nombre === undefined) {
-    res.status(400).json({
-      ok: false,
-      mensaje: 'El nombre es necesario'
-    });
-  } else {
-    res.json({
-      body
-    });
-  }
-});
+app.use(require('./routes/usuario'));
 
-app.put('/usuario/:id', function (req, res) {
-  let id = req.params.id;
-  res.json({
-    id
-  });
-});
+mongoose.connect(process.env.URL_DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}, (err, res)=> {
+  if (err) throw err;
 
-app.delete('/usuario', function (req, res) {
-  res.json('delete Usuario');
+  console.log('Base de datos ONLINE');
 });
  
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando el puerto: ', process.env.PORT);
+  console.log('Escuchando el puerto: ', process.env.PORT);
 });
